7f13e1593f08d942849706f34b687022735a78c3
Felipe Hamann <felipe.hamann@remotatec.com.br>
2026-09-02 08:49:32 -0300

    Adicionado card para visualização de artistas

---
+++ ScreenSound.Web/Layout/ScreendSoundPallete.cs
@@ -0,0 +1,17 @@
+﻿using MudBlazor;
+using MudBlazor.Utilities;
+
+namespace ScreenSound.Web.Layout
+{
+    public class ScreendSoundPallete : PaletteDark
+    {
+        private ScreendSoundPallete()
+        {
+            Primary = new MudColor("#9966FF");
+            Secondary = new MudColor("#F6AD31");
+            Tertiary = new MudColor("#8AE491");
+        }
+
+        public static ScreendSoundPallete CreatePallete => new();
+    }
+}
