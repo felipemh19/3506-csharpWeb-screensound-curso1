ef3d4bfb74018e822272e74cbf5302fa432d932b
Felipe Hamann <felipe.hamann@remotatec.com.br>
2026-09-08 08:43:27 -0300

    Criados comandos e componentes para edição e exclusão de artistas

---
+++ ScreenSound.Web/Requests/ArtistaRequestEdit.cs
@@ -0,0 +1,5 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ScreenSound.Web.Requests;
+
+public record ArtistaRequestEdit(int Id, [Required] string Nome, string Bio) : ArtistaRequest(Nome, Bio);
--- ScreenSound.Web/Services/ArtistaAPI.cs
+++ ScreenSound.Web/Services/ArtistaAPI.cs
@@ -18,9 +18,24 @@ namespace ScreenSound.Web.Services
             return await _httpClient.GetFromJsonAsync<ICollection<ArtistaResponse>>("artistas");
         }
 
-        public async Task CreateArtista(ArtistaRequest artista)
+        public async Task<ArtistaResponse?> GetArtistaByNomeAsync(string nome)
+        {
+            return await _httpClient.GetFromJsonAsync<ArtistaResponse>($"artistas/{nome}");
+        }
+
+        public async Task CreateArtistaAsync(ArtistaRequest artista)
         {
             await _httpClient.PostAsJsonAsync($"artistas", artista);
+        }
+
+        public async Task UpdateAsystaAsync(ArtistaRequestEdit artista)
+        {
+            await _httpClient.PutAsJsonAsync($"artistas/{artista.Id}", artista);
+        }
+
+        public async Task DeleteArtistaAsync(int id)
+        {
+            await _httpClient.DeleteAsync($"artistas/{id}");
         }
     }
 }
